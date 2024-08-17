582fb6cba3c2d6aed1e288a517f80b658c0185a1
varunschaudhari <varunschaudhari@gmail.com>
2024-08-17 11:00:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'); // Imports the express
+const app = new express(); // creates and instance of express application that will use to define servers behaviour
+// `app` object represents web application and provides methids for defining routes, configuring middleware , handling re and res
+
+const config = require('./utils/config');
+
+app.use(express.json()); // This middleware parses incoming JSON requests and puts the parsed data into req.body
+app.use(express.urlencoded({ extended: true })); // express.urlencoded() is another built-in middleware function in Express that is used to parse incoming requests with URL-encoded payloads.
+// extened: true --> to parse the nested objects
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ utils/config.js
@@ -0,0 +1,6 @@
+const config = {
+    PORT: process.env.PORT || 5000,
+};
+
+
+module.exports = {config};
